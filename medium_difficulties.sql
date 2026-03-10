e6125027413913fade3f4bb27d75e52a716558ad
Abhishek <83511097+Abhishek786-dev@users.noreply.github.com>
2026-03-10 12:37:00 +0530

    Create medium_difficulties.sql

---
+++ medium_difficulties.sql
@@ -0,0 +1,12 @@
+-- 1.
+-- Show patient_id, first_name, last_name from patients whos diagnosis is 'Dementia'.
+-- Primary diagnosis is stored in the admissions table.
+--> 
+
+select
+  patients.patient_id,
+  first_name,
+  last_name
+from patients
+  INNER JOIN admissions on patients.patient_id = admissions.patient_id
+where diagnosis is 'Dementia'
