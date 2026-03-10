31aa64b9fe015149b852dff513ebc654091b87ad
Abhishek <83511097+Abhishek786-dev@users.noreply.github.com>
2026-03-10 18:59:51 +0530

    Update medium_difficulties.sql

--- medium_difficulties.sql
+++ medium_difficulties.sql
@@ -10,3 +10,57 @@ select
 from patients
   INNER JOIN admissions on patients.patient_id = admissions.patient_id
 where diagnosis is 'Dementia'
+
+-- 2.
+-- Display every patient's first_name.
+-- Order the list by the length of each name and then by alphabetically.
+
+SELECT first_name
+FROM patients
+order by
+  len(first_name),
+  first_name;
+
+-- 3.
+-- Show the total amount of male patients and the total amount of female patients in the patients table.
+-- Display the two results in the same row.
+
+select 
+  sum(case when gender = 'M' then 1 end) as male_count,
+  sum(case when gender = 'F' then 1 end) as female_count 
+from patients;
+
+-- OR
+
+SELECT 
+  SUM(Gender = 'M') as male_count, 
+  SUM(Gender = 'F') AS female_count
+FROM patients
+
+-- 4.
+-- Show first and last name, allergies from patients which have allergies to either 'Penicillin' or 'Morphine'. 
+-- Show results ordered ascending by allergies then by first_name then by last_name.
+
+SELECT
+  first_name,
+  last_name,
+  allergies
+FROM patients
+WHERE
+  allergies IN ('Penicillin', 'Morphine')
+ORDER BY
+  allergies ASC,
+  first_name ASC,
+  last_name ASC;
+
+-- 5.
+-- Show patient_id, diagnosis from admissions. Find patients admitted multiple times for the same diagnosis.
+
+SELECT
+  patient_id,
+  diagnosis
+FROM admissions
+GROUP BY
+  patient_id,
+  diagnosis
+HAVING COUNT(*) > 1;
